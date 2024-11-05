0f6d3beca95ecb25f32b7a9fc75acc1b85fbd6f3
fengshao <1702526238@qq.com>
2024-11-05 22:27:21 +0800

    2024_11_5

--- study java/topic/src/P126_T3.java
+++ study java/topic/src/P126_T3.java
@@ -3,21 +3,21 @@ public class P126_T3 {
     public static void main(String[]args){
         Scanner scanner=new Scanner(System.in);
         System.out.println("请输入国际标准书号");
-        String bookNumber=scanner.nextLine();
+        String bookNumber=scanner.nextLine();//获取书号
         if(bookNumber.length()!=17){
-            System.out.println("该书号不合法");
+            System.out.println("该书号不合法");//判断书号长度，不对的直接判定不合法
         }
         else{
-            String[]test=bookNumber.split("-");
+            String[]test=bookNumber.split("-");//定义字符串数组并将原数组分割后传入字符串数组
             if(test.length!=5){
-                System.out.println("该书号不合法");
+                System.out.println("该书号不合法");//假设分离后并不是五个部分就判定不合法
             }
             else {
                 if(test[0].length()!=3|test[1].length()!=1|test[2].length()!=3|test[3].length()!=5|test[4].length()!=1){
-                    System.out.println("该书号不合法");
+                    System.out.println("该书号不合法");//如果某一个部分的长度不正确就判定不合法
                 }
                 else{
-                    System.out.println("该书号合法");
+                    System.out.println("该书号合法");//上述情况都不符合就判定合法
                 }
             }
         }
---
+++ study java/topic/src/P126_T4.java
@@ -0,0 +1,18 @@
+public class P126_T4 {
+    public static int countLetters(String s){
+        int wordNumber=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)>='a'&s.charAt(i)<='z'){//运用Ascll码表，在a到z区间进行判定
+                wordNumber+=1;
+            }
+            if(s.charAt(i)>='A'&s.charAt(i)<='Z'){//运用Ascll码表，在大写字母A到Z区间判定
+                wordNumber+=1;
+            }
+        }
+        return wordNumber;
+    }
+
+    public static void main(String[]args){
+        System.out.println(countLetters("Beijing 2022"));
+    }
+}
